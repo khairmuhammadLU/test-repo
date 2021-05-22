b4700e57f9d4686d4a948a9c07f16eb5beb96430
khairmuhammadLU <khair.muhammad@logic-unit.com>
2021-05-22 17:50:31 +0500

    add new file with code

---
+++ src/components/pages/newfile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class newfile extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
